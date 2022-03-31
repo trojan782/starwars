6b4114eed5b7684282eabca43cac29bd2ea6c11b
trojancodes <davidjacobs522@gmail.com>
2022-03-31 02:47:11 +0100

    added the update model

--- models/index.py
+++ models/index.py
@@ -1,12 +1,36 @@
 from pydantic import BaseModel
-# from typing import Optional
+from typing import Optional
+from bson import ObjectId
 
 class Favorite(BaseModel):
     type: str # 'movie' || 'character'
     name: str
     url: str
     
-    
+    class Config:
+        schema_extra = {
+            "example": {
+                "type": "movie",
+                "name": "A New Hope",
+                "url" : "https://swapi.dev/api/films/1/"
+            }
+        }
+class updateFavoriteModel(BaseModel):
+    type: Optional[str]
+    name: Optional[str]
+    url: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "type": "movie",
+                "name": "A New Hope",
+                "url": "https://swapi.dev/api/films/1/"
+            }
+        }
+
 def ResponseModel(data, message):
     return {
         "data": [data],
