5e96bad4e9900d542ba67d4f02c4ee5e75f74bcc
trojancodes <davidjacobs522@gmail.com>
2022-04-06 09:52:24 +0100

    completed models

--- models/index.py
+++ models/index.py
@@ -1,8 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from bson import ObjectId
 
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 class Favorite(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     type: str # 'movie' || 'character'
     name: str
     url: str
