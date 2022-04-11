04efa664238560ba989897ae1a172d65b55adf47
trojancodes <davidjacobs522@gmail.com>
2022-04-12 00:56:54 +0100

    working..

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from urllib import response
 from utils.config import get_settings, Settings
-from pydantic import BaseModel
-from fastapi import FastAPI, Depends, HTTPException 
-from models.index import Favorite
+# from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, status, Body
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from models import index
 from fastapi.middleware.cors import CORSMiddleware
+from db import database
 import requests
-
+from models.index import Favorite, ResponseModel
+from repository import FavoriteRepo
 app = FastAPI()
+#create the database tables on app startup or reload
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,9 +28,13 @@ async def ping(settings: Settings = Depends(get_settings)):
         "environment": settings.environment, 
         "testing": settings.testing
     }
-    
+@app.get('/', response_description="Home")
+async def welcome():
+    return {"message": "Welcome to our starwars favorite, kindly explore all your starwars related things!", 
+            "link": "http://127.0.0.1:8000/docs"}
+
 # endpoint to return all people
-@app.get('/people')
+@app.get('/people', response_description="Return all people")
 async def get_people():
     try:
        url = 'https://swapi.dev/api/people'
@@ -33,8 +43,22 @@ async def get_people():
     except HTTPException:
        return {'message': 'Something went please wrong, try again'}
 
+
+# To retrive a single person
+@app.get("/people/{id}", response_description="Get a single person")
+async def get_person(id):
+    try:
+        url = f'https://swapi.dev/api/people/{id}'
+        person = requests.get(url)
+        if person is not None:
+            return person.json()
+    except:
+        raise HTTPException(status_code=404, detail=f"Person {id} not found")
+        
+        
+    
 # endpoint to return all movies
-@app.get('/films')
+@app.get('/films', response_description="Return all movies")
 async def get_movies():
     try:
         url = 'https://swapi.dev/api/films'
@@ -51,4 +75,24 @@ async def get_fav():
 
 
 
-# mongodb+srv://trojan1:<password>@cluster0.uaj88.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+# @app.get("/favorite", response_model=ResponseModel)
+# async def get_all():
+#     _favlist = await FavoriteRepo.get_fav()
+#     print(_favlist)
+#     return ResponseModel(code=200, status="Ok", message="Success retrieve all data", result=_favlist).dict(exclude_none=True)
+
+@app.get("/home", response_description= "Retrive all favorites")
+async def get_favorites():
+    favs = await FavoriteRepo.retrive_favorites()
+    if favs:
+        return ResponseModel(favs, "Students retrived successfully")
+    return ResponseModel(favs, "empty list")
+        
+# @app.post("/favorite", response_description="Get all favs", response_model=ResponseModel)
+# async def create_fav(fav: Favorite = Body(...)):
+#     fav = jsonable_encoder(fav)
+#     new_fav = await database["favorites"].insert_one(fav)
+#     created_fav = await database["favorites"].find_one({"_id": new_fav.inserted_id})
+#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_fav)
+
+
