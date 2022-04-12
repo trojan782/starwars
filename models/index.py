e39192782d3cc257ef649b01e2b1f8bbaa8594a6
trojancodes <davidjacobs522@gmail.com>
2022-04-12 01:30:05 +0100

    working on models

--- models/index.py
+++ models/index.py
@@ -1,25 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import Optional
-from bson import ObjectId
+from typing import Optional, TypeVar
 
-
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
 class Favorite(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    type: str # 'movie' || 'character'
+    id: str = None
+    type: str
     name: str
     url: str
     
@@ -36,16 +20,16 @@ class updateFavoriteModel(BaseModel):
     name: Optional[str]
     url: Optional[str]
 
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "type": "movie",
-                "name": "A New Hope",
-                "url": "https://swapi.dev/api/films/1/"
-            }
-        }
+#     class Config:
+#         arbitrary_types_allowed = True
+#         json_encoders = {ObjectId: str}
+#         schema_extra = {
+#             "example": {
+#                 "type": "movie",
+#                 "name": "A New Hope",
+#                 "url": "https://swapi.dev/api/films/1/"
+#             }
+#         }
 
 def ResponseModel(data, message):
     return {
@@ -53,3 +37,4 @@ def ResponseModel(data, message):
         "code": 200,
         "message": message,
     }
+    
